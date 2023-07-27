9195e5559bb71fed6bcf5a7be21612c530389105
Amir Samoha <amir.samoha42@gmail.com>
2023-07-27 13:15:19 +0300

    ..

--- src/componnets/Navbar.jsx
+++ src/componnets/Navbar.jsx
@@ -5,23 +5,25 @@ import { Link } from "react-router-dom";
 import { logo } from "../utils/constants";
 import Searchbar from "./Searchbar";
 
-const Navbar = () => (
-  <Stack
-    direction="row"
-    alignItems="center"
-    p={2}
-    sx={{
-      position: "sticky",
-      background: "#000",
-      top: 0,
-      justifyContent: "space-between",
-    }}
-  >
-    <Link to="/" style={{ display: "flex", alignItems: "center" }}>
-      <img src={logo} alt="logo" height={45} />
-    </Link>
+const Navbar = () => {
+  return (
+    <Stack
+      direction="row"
+      alignItems="center"
+      p={2}
+      sx={{
+        position: "sticky",
+        background: "#000",
+        top: 0,
+        justifyContent: "space-between",
+      }}
+    >
+      <Link to="/" style={{ display: "flex", alignItems: "center" }}>
+        <img src={logo} alt="logo" height={45} />
+      </Link>
 
-    <Searchbar />
-  </Stack>
-);
+      <Searchbar />
+    </Stack>
+  );
+};
 export default Navbar;
